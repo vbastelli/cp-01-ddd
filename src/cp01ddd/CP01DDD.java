b31744d1280082a0c12c884cc096e8dce94ed4c1
victoriobastelli <162315108+vbastelli@users.noreply.github.com>
2025-03-17 09:03:59 -0300

    Update CP01DDD.java

--- src/cp01ddd/CP01DDD.java
+++ src/cp01ddd/CP01DDD.java
@@ -6,16 +6,13 @@ public class CP01DDD {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Variáveis para armazenar os dados
         String placa = null;
         int horasEstacionado = 0;
         double valorTotal;
         boolean carroRegistrado = false;
 
-        // Loop principal do menu
         boolean sistemaAtivo = true;
         while (sistemaAtivo) {
-            // Exibe o menu para o usuário
             System.out.println("\n--- Sistema de Estacionamento ---");
             System.out.println("1. Consultar carro");
             System.out.println("2. Saída de carro");
@@ -23,10 +20,10 @@ public class CP01DDD {
             System.out.println("4. Sair");
             System.out.print("Escolha uma opção: ");
             int opcao = sc.nextInt();
-            sc.nextLine();  // Limpar o buffer de entrada
+            sc.nextLine();
 
             switch (opcao) {
-                case 1:  // Consultar carro
+                case 1:
                     if (carroRegistrado) {
                         System.out.println("Placa: " + placa);
                         System.out.println("Tempo de estacionamento: " + horasEstacionado + " horas");
@@ -36,7 +33,7 @@ public class CP01DDD {
                     }
                     break;
 
-                case 2:  // Saída de carro
+                case 2:
                     if (carroRegistrado) {
                         carroRegistrado = false;
                         System.out.println("Carro com placa " + placa + " removido do estacionamento.");
@@ -47,7 +44,7 @@ public class CP01DDD {
                     }
                     break;
 
-                case 3:  // Registrar carro
+                case 3:
                     if (carroRegistrado) {
                         System.out.println("Carro já registrado.");
                         break;
@@ -57,18 +54,17 @@ public class CP01DDD {
                     placa = sc.nextLine();
                     System.out.print("Digite o número de horas que o carro ficou estacionado: ");
                     horasEstacionado = sc.nextInt();
-                    sc.nextLine(); // Limpar o buffer
+                    sc.nextLine();
 
                     valorTotal = horasEstacionado * 15.0;
                     carroRegistrado = true;
 
-                    // Exibe as informações
                     System.out.println("Placa do carro: " + placa);
                     System.out.println("Tempo de estacionamento: " + horasEstacionado + " horas");
                     System.out.println("Valor a pagar: R$ " + valorTotal);
                     break;
 
-                case 4:  // Sair
+                case 4:
                     sistemaAtivo = false;
                     System.out.println("Sistema de Estacionamento encerrado.");
                     break;
